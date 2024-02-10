177d34f2d32ab71934706ba36170ff3b4b4f238a
Emisael Kisler <emisael@croney.io>
2024-02-10 19:02:16 -0400

    Agregando bloques e identacion y condicional if

---
+++ 14bloques_e_identacion.py
@@ -0,0 +1,10 @@
+numero = 5
+
+if numero > 0:
+  print("El número es positivo")
+
+else:
+  print("El número es cero o negativo")
+  print("El número es cero o negativo")
+
+print("El número es cero o negativo")
