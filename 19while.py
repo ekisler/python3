cceccbfd2f2848ffec1b10bf5e7396de287d197f
Emisael Kisler <emisael@croney.io>
2024-02-12 09:07:10 -0400

    Trabajamdo con while y try-except

---
+++ 19while.py
@@ -0,0 +1,11 @@
+print("*********De menor a mayor**********")
+numero = 1
+while numero <= 10:
+  print(numero)
+  numero += 1
+
+print("*********De mayor a menor**********")
+numero1 = 10
+while numero1 >= 1:
+  print(numero1)
+  numero1 -= 1
